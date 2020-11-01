fa6d345eab68aecfd97da4e0a3e0a07f218b4b57
Victor Glebov <glebovv3105@gmail.com>
2020-11-01 13:24:18 -0700

    Added Station and Route models

--- bus_navigation/models.py
+++ bus_navigation/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    routes = models.ManyToManyField(related_name='stations')
+    name = models.CharField(max_length=50)
+
+
+class Route(models.Model):
+    name = models.CharField(max_length=50)
